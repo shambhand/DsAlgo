c441a256dd1945cfc10baf137a01ad2884626111
ShamBhand <sbhand@equalexperts.com>
2020-08-12 14:11:51 +0530

    LeetCodeProblem1: optimized two sum implementation using HashMap

--- leetcode/src/main/java/easy/TwoSum.java
+++ leetcode/src/main/java/easy/TwoSum.java
@@ -1,26 +1,23 @@
 package easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Problem Statement : https://leetcode.com/problems/two-sum
  */
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-        int i, j, sum = 0;
-        boolean meet = false;
-        for (i = 0; i < nums.length; i++) {
-            for (j = i + 1; j < nums.length; j++) {
-                sum = nums[i] + nums[j];
-                if (sum == target) {
-                    meet = true;
-                    break;
-                }
-            }
-            if (meet) {
-                result[0] = i;
-                result[1] = j;
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                result[0] = map.get(diff);
+                result[1] = i;
                 break;
             }
+            map.put(nums[i], i);
         }
         return result;
     }
